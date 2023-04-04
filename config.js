aa9293c8aefc3b48d5e032d0fb76c268e8350e36
Chino-chan <tungchaphet2512@gmail.com>
2023-04-04 21:39:29 +0700

    fix: config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const NodeCache = require("node-cache")
 
 const config = {
-    pageToken: 'EAAK4LazBcpABAJC3Rfjmvd28ZBf9uoEiGH2ayShbaZBnLkqvwb1HQeC76ZCDXtExZBdq0h07ZBKLRwpgGwPQjtimHLrZAIQOZC2wv2MHcl0dwyC4WZCr1rdRI3AJ1Ejwvo4k0pEAIUfvyUCyAdjIY1EyiuUFtT4CkP5FzsZCd797ZCP4ctf4d4zA0hhSvZC6bSRPVNWKgibCy9grQZDZD',
-    appSecret: '302dd4e605dd26deb0c7f5c9c7f17a0f',
-    verifyToken: 'chinochinochinochinochinochinochinochinochinochino',
+    pageToken: '', // Đưa token của fanpage của bạn vô đây.
+    appSecret: '', // Cái này có thể có hoặc không, nếu có thì nó càng bảo mật.
+    verifyToken: '', // Nhập cái gì vô đây cũng được nhưng nhớ copy vào Verify Token ở mục Webhooks.
     openai: {
-        model: '',
-        token: '',
-        max_tokens: 2048
+        model: '', // Model ChatGPT (gpt-3-turbo, gpt-3, gpt-4)
+        token: '', // API Token của ChatGPT
+        max_tokens: 2048 // Bạn có thể thay đổi theo bạn muốn.
     },
     baseURL: 'https://graph.facebook.com/v16.0',
     ratelimit: new Map(),
